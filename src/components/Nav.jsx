8eb776367a7873edc0892bb11cc320bc6ded09bc
Richard <richardvu93@gmail.com>
2024-01-02 16:01:15 -0800

    Initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,24 @@
+const Nav = () => {
+  return (
+    <nav className='nav'>
+      <div className='nav__container'>
+        <div className='nav__logo'>Richard.dev</div>
+        <div className='nav__links'>
+          <a href='' className='nav__link'>
+            Home
+          </a>
+          <a href='' className='nav__link'>
+            About
+          </a>
+          <a href='' className='nav__link'>
+            Projects
+          </a>
+          <a href='' className='nav__link'>
+            Contact
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Nav;
