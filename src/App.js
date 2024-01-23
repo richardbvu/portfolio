6a084a599d448d32c772ec20cddcdbbdbb0c2283
Richard <richardvu93@gmail.com>
2024-01-22 18:59:38 -0800

    remove router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import { Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
+import About from "./components/About";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Nav />
-      {/* <Routes> */}
-      {/* <Route></Route> */}
-      {/* </Routes> */}
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
     </>
   );
 }
